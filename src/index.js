28de4998447e20715c79828d3697f96ffc5ef2c5
rubythalib33 <abdullah3333abdul@gmail.com>
2019-12-18 17:30:14 +0700

    mantul

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./assets/style.css";
+import quizService from './quizService';
+import QuestionBox from "./component/QuestionBox";
+import Result from "./component/Result"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class QuizBee extends Component{
+    state={
+        questionBank:[],
+        score:0,
+        responses:0,
+    }
+    getQuestion = () =>{
+        quizService().then(question=>{
+            this.setState({
+                questionBank: question
+            })
+        })
+    }
+    componentDidMount(){
+        this.getQuestion();
+    }
+    computeAnswer= (answer, correctAnswer)=>{
+        if(answer===correctAnswer) this.setState({
+            score:this.state.score+1
+        })
+        this.setState({
+            responses:this.state.responses<5 ? this.state.responses+1:5
+        })
+    }
+    playAgain = () =>{
+        this.setState({
+            score:0,
+            responses:0
+        })
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 && this.state.responses<5 && this.state.questionBank.map(
+                    ({question,answers,correct,questionId})=>(
+                        <QuestionBox 
+                            question={question} 
+                            options={answers} 
+                            key={questionId} 
+                            selected={answer=>this.computeAnswer(answer, correct)}/>
+                    )
+                )}
+                {this.state.responses===5 ? <Result score={this.state.score} playAgain={this.playAgain} /> : null}
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<QuizBee />,document.getElementById("root"));
